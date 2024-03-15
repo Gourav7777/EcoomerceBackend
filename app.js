ca065566a70e809a6e6ed159f7d61194112af301
GouravAccel <gourav.bundiwal@accelgrowthtech.com>
2024-03-15 17:44:42 +0530

    chc

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 const express = require('express');
-const { connection } = require("./db")
-require("dotenv").config()
+const { connection } = require("./db");
+require("dotenv").config();
 const PORT = process.env.PORT || 8000;
-const cors = require ("cors")
-const {productroute} = require("./Routes/product.routes")
-const {cartroute} = require("./Routes/cart.routes")
+const cors = require("cors");
+const { productroute } = require("./Routes/product.routes");
+const { cartroute } = require("./Routes/cart.routes");
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.use('/products',productroute)
-
-app.use('/cart',cartroute)
+app.use('/products', productroute);
+app.use('/cart', cartroute);
 
 app.get('/', (req, res) => {
     res.send('Welcome to my Node.js Express app!');
-  });
+});
 
-
-app.listen(PORT, async() => {
-  try {
-        
-    await connection
-    console.log("connected to mongo")
-    console.log(`Server is running on port ${PORT}`);
-
-    } catch (error) {
-        console.log(error.message)
-    }
+// Call connection function before listen
+connection.then(() => {
+    app.listen(PORT, () => {
+        console.log("Connected to MongoDB");
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
 });
